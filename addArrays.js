cc29daf6df5b17c40d321cab0785cdc2b48e9356
root <root@Atlas.localdomain>
2024-02-26 23:37:02 -0500

    Added function

--- addArrays.js
+++ addArrays.js
@@ -1,4 +1,8 @@
 //Write a function combineArray(array1, array2) that takes in two arrays of numbers and returns the two arrays combined into a single array.
 
+const combineArray = (array1, array2) => {
+
+}
+
 console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
 console.log(combineArray([17, 5], [6, 7]));  // => [17, 5, 6, 7]
