fa53a029e802053828d88c86bb73dc0840df3983
sebastian <stmr.tay@gmail.com>
2024-02-26 23:43:52 -0500

    Solved addArrays by spreading both into a new array

--- addArrays.js
+++ addArrays.js
@@ -1,7 +1,7 @@
 //Write a function combineArray(array1, array2) that takes in two arrays of numbers and returns the two arrays combined into a single array.
 
 const combineArray = (array1, array2) => {
-    l
+    return [...array1, ...array2];
 }
 
 console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
