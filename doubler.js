66ca9758e8a0fe5d3dc39967d787f34826981559
sebastian <stmr.tay@gmail.com>
2024-02-27 00:46:35 -0500

    solved doubler using map method

--- doubler.js
+++ doubler.js
@@ -1,4 +1,8 @@
 //Write a function doubler(numbers) that takes an array of numbers and returns a new array where every element of the original array is multiplied by 2.
 
+const doubler = numbers => {
+    return numbers.map(num => num * 2);
+}
+
 console.log(doubler([1, 2, 3, 4])); // => [2, 4, 6, 8]
 console.log(doubler([7, 1, 8])); // => [14, 2, 16]
